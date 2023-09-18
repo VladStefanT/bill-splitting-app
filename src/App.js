2ed9eb63b43b746ca4acc3cda612e291e776d826
Tudorache Vlad <vladstefan8938@gmail.com>
2023-09-18 11:10:47 +0300

    Refactored Friend component

--- src/App.js
+++ src/App.js
@@ -35,27 +35,40 @@ function FriendsList() {
   return (
     <ul>
       {friends.map((friend) => (
-        <Friend friend={friend} key={friend.id}>
-          <img src={friend.image} alt={friend.name}></img>
-          <h3>{friend.name}</h3>
-          {friend.balance < 0 && (
-            <p className="red">
-              You owe {friend.name} {Math.abs(friend.balance)}$
-            </p>
-          )}
-          {friend.balance > 0 && (
-            <p className="green">
-              {friend.name} owes you {Math.abs(friend.balance)}$
-            </p>
-          )}
-          {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-          <button className="button">Select</button>
-        </Friend>
+        <Friend friend={friend} key={friend.id} />
       ))}
     </ul>
   );
 }
 
-function Friend({ friend, children }) {
-  return <li>{children}</li>;
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name}></img>
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <button className="button">Select</button>
+    </li>
+  );
+}
+
+function FormAddFriend() {
+  return (
+    <>
+      <form className="form-add-friend">
+        <label>Friend name</label>
+        <input type="text"></input>
+      </form>
+    </>
+  );
 }
