bbb9d7d37fec83b16962e9d8750c59a982bb630a
Tudorache Vlad <vladstefan8938@gmail.com>
2023-09-18 11:08:30 +0300

    Finished friend list

--- src/App.js
+++ src/App.js
@@ -30,5 +30,32 @@ export default function App() {
 }
 
 function FriendsList() {
-  return <ul>List</ul>;
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id}>
+          <img src={friend.image} alt={friend.name}></img>
+          <h3>{friend.name}</h3>
+          {friend.balance < 0 && (
+            <p className="red">
+              You owe {friend.name} {Math.abs(friend.balance)}$
+            </p>
+          )}
+          {friend.balance > 0 && (
+            <p className="green">
+              {friend.name} owes you {Math.abs(friend.balance)}$
+            </p>
+          )}
+          {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+          <button className="button">Select</button>
+        </Friend>
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend, children }) {
+  return <li>{children}</li>;
 }
